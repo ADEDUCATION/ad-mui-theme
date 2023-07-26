59ec29563a85c2285184a71cfcf07dc9b41e6841
1ouiss <ls.bouet@gmail.com>
2023-07-26 16:05:55 +0200

    add variant onChip

--- src/newTheme/Components/dataDisplay.ts
+++ src/newTheme/Components/dataDisplay.ts
@@ -42,12 +42,72 @@ const dataDisplay = ({ palette, typography }: Props) => ({
     },
     variants: [
       {
-        props: { size: 'large' },
+        props: { size: 'small' },
         style: {
+          '& .MuiAvatar-root': {
+            width: "18px",
+            height: "18px",
+            fontSize: typography?.bodyMedium.fontSize,
+            marginLeft: "3px",
+          },
+          "& .MuiSvgIcon-root": {
+            width: "18px",
+            height: "18px",
+            marginLeft: "3px",
+          },
+          "& .MuiChip-deleteIcon": {
+            width: "18px",
+            height: "18px",
+            marginRight: "3px",
+            marginLeft: "-4px",
+          },
+        }
+      },
+      {
+        props: { size: 'medium' },
+        style: {
+          display: 'inline-flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: "27px",
           '& .MuiChip-label': {
+            fontSize: typography?.bodyMedium.fontSize,
+            fontWeight: typography?.bodyMedium.fontWeight,
+            padding: "0 10px",
+          },
+          '& .MuiAvatar-root': {
+            width: "21px",
+            height: "21px",
+            fontSize: typography?.bodyMedium.fontSize,
+            marginLeft: "3px",
+          },
+          "& .MuiSvgIcon-root": {
+            width: "21px",
+            height: "21px",
+            marginLeft: "3px",
+          },
+          "& .MuiChip-deleteIcon": {
+            width: "21px",
+            height: "21px",
+            marginRight: "3px",
+            marginLeft: "-4px",
           }
         }
-      }
+      },
+      {
+        props: { size: 'large' },
+        style: {
+          display: 'inline-flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: "32px",
+          '& .MuiChip-label': {
+            fontSize: typography?.subtitleMedium.fontSize,
+            fontWeight: typography?.subtitleMedium.fontWeight,
+            padding: "0 12px",
+          }
+        }
+      },
     ],
   },
 });
