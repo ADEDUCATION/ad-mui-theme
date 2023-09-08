0bcc351fc78b1ca6558710f4aafac43cd46c91d2
1ouiss <ls.bouet@gmail.com>
2023-09-08 16:24:52 +0200

    add radius color and new typo

--- src/newTheme/Components/components.ts
+++ src/newTheme/Components/components.ts
@@ -1,12 +1,12 @@
-import { OverridesStyleRules } from '@mui/material/styles/overrides';
+import { OverridesStyleRules } from "@mui/material/styles/overrides";
 
-import { CombinedPalette, SubObjectKeys } from '../Palette/utils';
-import typographyImports from '../Typography/typographyImports';
-import { SubObjectKeysTypography, TypographyMerge } from '../Typography/utils';
-import dataDisplay from './dataDisplay';
-import globals from './globals';
-import inputs from './inputs';
-import { layout } from './layout';
+import { CombinedPalette, SubObjectKeys } from "../Palette/utils";
+// import typographyImports from "../Typography/typographyImports";
+import { SubObjectKeysTypography, TypographyMerge } from "../Typography/utils";
+import dataDisplay from "./dataDisplay";
+import globals from "./globals";
+import inputs from "./inputs";
+import { layout } from "./layout";
 
 export type Props = {
   palette: CombinedPalette[SubObjectKeys] | undefined;
@@ -16,7 +16,12 @@ export type Props = {
 const components = ({ palette, typography }: Props): OverridesStyleRules => {
   return {
     MuiCssBaseline: {
-      styleOverrides: typographyImports,
+      styleOverrides: {
+        "@global": {
+          "@import":
+            'url("https://fonts.googleapis.com/css2?family=Barlow:wght@300;400;500;600;700&display=swap");',
+        },
+      },
     },
     ...inputs({ palette, typography }),
     ...dataDisplay({ palette, typography }),
--- src/newTheme/theme.ts
+++ src/newTheme/theme.ts
@@ -1,47 +1,61 @@
-import { PaletteOptions, createTheme } from '@mui/material';
-import { frFR as coreFrFR } from '@mui/material/locale';
-import { frFR as gridfrFR } from '@mui/x-data-grid';
-import { frFR } from '@mui/x-date-pickers';
-import { frFR as pickersfrFR } from '@mui/x-date-pickers';
+import { PaletteOptions, createTheme } from "@mui/material";
+import { frFR as coreFrFR } from "@mui/material/locale";
+import { frFR as gridfrFR } from "@mui/x-data-grid";
+import { frFR } from "@mui/x-date-pickers";
+import { frFR as pickersfrFR } from "@mui/x-date-pickers";
 
-import themeTokens from '../tokens/tokens.json';
-import components from './Components/components';
-import createNewPalette from './Palette/createPalette';
-import { CombinedPalette } from './Palette/utils';
-import createNewTypography from './Typography/createTypography';
+import themeTokens from "../tokens/tokens.json";
+import components from "./Components/components";
+import createNewPalette from "./Palette/createPalette";
+import { CombinedPalette } from "./Palette/utils";
+import createNewTypography from "./Typography/createTypography";
 
 export const tokens = {
   esp: themeTokens.themes.esp,
   ade: themeTokens.themes.ade,
 };
 
-declare module '@mui/material/styles' {
+export interface BorderRadius {
+  borderRadius: number;
+  buttonRadius: number;
+  inputRadius: number;
+  smallRadius: number;
+  mediumRadius: number;
+  largeRadius: number;
+}
+
+declare module "@mui/material/styles" {
   interface Theme {
     ade: PaletteOptions;
     esd: PaletteOptions;
     adeDark: PaletteOptions;
     palette: CombinedPalette[keyof typeof tokens];
+    shape: BorderRadius;
   }
 }
-declare module '@mui/material/Button' {
+declare module "@mui/material/Button" {
   interface ButtonPropsColorOverrides {
     color1: true;
     color3: true;
   }
 }
 
-declare module '@mui/material/Typography' {
+declare module "@mui/material/Typography" {
   interface TypographyPropsVariantOverrides {
     subtitleBold: true;
+    subtitleSemiBold: true;
     subtitleMedium: true;
+    subtitleRegular: true;
     subtitleLight: true;
     bodyBold: true;
+    bodySemiBold: true;
     bodyMedium: true;
+    bodyRegular: true;
     bodyLight: true;
   }
 }
 
-declare module '@mui/material/Chip' {
+declare module "@mui/material/Chip" {
   interface ChipPropsColorOverrides {
     color1: true;
     color2: true;
@@ -58,7 +72,7 @@ declare module '@mui/material/Chip' {
   }
 }
 
-declare module '@mui/material/IconButton' {
+declare module "@mui/material/IconButton" {
   interface IconButtonPropsColorOverrides {
     color1: true;
     color2: true;
@@ -73,9 +87,33 @@ declare module '@mui/material/IconButton' {
   }
 }
 
+const createNewShape = (mode: keyof typeof tokens) => {
+  if (tokens[mode]) {
+    const borderRadius = tokens[mode].shape.borderRadius;
+    const buttonRadius = tokens[mode].shape.buttonRadius;
+    const inputRadius = tokens[mode].shape.inputRadius;
+    const smallRadius = tokens[mode].shape.largeRadius;
+    const mediumRadius = tokens[mode].shape.mediumRadius;
+    const largeRadius = tokens[mode].shape.smallRadius;
+
+    return {
+      borderRadius,
+      buttonRadius,
+      inputRadius,
+      smallRadius,
+      mediumRadius,
+      largeRadius,
+    };
+  }
+};
+
 export const newTheme = (mode: keyof typeof tokens) => {
   const customPalette = createNewPalette(mode);
-  const customTypography = createNewTypography({ mode, palette: customPalette });
+  const customTypography = createNewTypography({
+    mode,
+    palette: customPalette,
+  });
+  const customShape = createNewShape(mode);
 
   return createTheme(
     {
@@ -92,12 +130,12 @@ export const newTheme = (mode: keyof typeof tokens) => {
       components: components({
         palette: customPalette,
         typography: customTypography,
-        
       }),
+      shape: customShape,
     },
     gridfrFR,
     frFR,
     pickersfrFR,
-    coreFrFR,
+    coreFrFR
   );
 };
