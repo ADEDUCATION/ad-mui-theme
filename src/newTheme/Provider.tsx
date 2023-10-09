f4c69f79f06b45e07d47a41ea4daf14dd9e491b4
1ouiss <ls.bouet@gmail.com>
2023-10-09 16:13:48 +0200

    remove comment and unused variable

--- src/newTheme/Provider.tsx
+++ src/newTheme/Provider.tsx
@@ -1,23 +1,15 @@
 import React from "react";
 import { ThemeProvider } from "@mui/material";
 
-// import useTheme from '@/store/theme';
-
-import { newTheme, tokens } from "./theme";
+import { newTheme } from "./theme";
 import { SchoolThemes } from "./types";
 
 type CustomThemeProviderProps = {
   children: JSX.Element;
   mode: SchoolThemes;
 };
-// declare module "@mui/material/styles" {
-//   interface Theme {
-//     palette: defaultTheme("ade");
-// }
 
 function CustomThemeProvider({ children, mode }: CustomThemeProviderProps) {
-  // const [theme] = useTheme();
-
   return <ThemeProvider theme={newTheme(mode)}>{children}</ThemeProvider>;
 }
 
