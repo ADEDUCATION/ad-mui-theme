be2ef851be14270045486d383203c90795359697
1ouiss <ls.bouet@gmail.com>
2023-11-17 10:31:29 +0100

    add required dot

--- storybook/src/CustomField/Label.tsx
+++ storybook/src/CustomField/Label.tsx
@@ -1,34 +1,29 @@
-import { Box, Tooltip, Typography } from "@mui/material";
+import { Box, Stack, Typography, useTheme } from "@mui/material";
 import { IFieldStructure } from "./type";
 
-const formColors: {
-  color: string;
-  id: "isCerfa" | "isConvention";
-  tooltip: string;
-}[] = [
-  {
-    color: "#f5b92a",
-    id: "isCerfa",
-    tooltip: "Champ nécessaire pour valider le formulaire Cerfa",
-  },
-  {
-    color: "#0795ff",
-    id: "isConvention",
-    tooltip: "Champ nécessaire pour valider le formulaire Convention",
-  },
-];
-
 const Label = ({ field }: { field: IFieldStructure }) => {
+  const theme = useTheme();
   return (
-    // <Box
-    //   alignContent="flex-end"
-    //   sx={{ pr: 1, fontSize: 12, fontWeight: "bold", color: "grey.300" }}
-    // >
-    //   {field.label}{" "}
-    // </Box>
-    <Typography variant="bodyBold" color="grey.300">
-      {field.label}
-    </Typography>
+    <Stack
+      direction="row"
+      alignItems="center"
+      gap={theme.space.xs}
+      mb={theme.space.xs}
+    >
+      <Typography variant="bodyBold" color="grey.300">
+        {field.label}
+      </Typography>
+      {field.mandatory && (
+        <Box
+          sx={{
+            width: theme.space["2xs"],
+            height: theme.space["2xs"],
+            backgroundColor: "warning.main",
+            borderRadius: theme.radius.shapeFull,
+          }}
+        ></Box>
+      )}
+    </Stack>
   );
 };
 
